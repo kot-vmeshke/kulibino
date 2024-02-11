5e2a155de7fbab0b9a93a617703c629de131fadb
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-02-11 17:01:24 +0100

    feat: add header, pages

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,14 @@
 console.log('Hello, Kulibino!');
+
+const handleMenu = () => {
+  const burgerButton = document.querySelector('#burger');
+  const menu = document.querySelector('.header__nav-list');
+
+  burgerButton.addEventListener('click', () => {
+    menu.classList.toggle('js-opened');
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleMenu();
+})
